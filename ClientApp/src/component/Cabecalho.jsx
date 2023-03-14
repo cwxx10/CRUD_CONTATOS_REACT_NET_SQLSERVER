9cda61e4dba7c48e555443e36a7adb9302a8005a
ciro.carvalho <ciro.carvalho@capgemini.com>
2023-03-14 18:43:45 -0300

    MÉTODO POST FINALIZADO

--- ClientApp/src/component/Cabecalho.jsx
+++ ClientApp/src/component/Cabecalho.jsx
@@ -4,27 +4,42 @@
 const Cabecalho = ({ handleGuardarContato }) => {
     const [nome, setNome] = useState('');
     const [email, setEmail] = useState('');
-    const [telefone, setTelefone] = useState('');
+    const [phone, setPhone] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const contato = { nome, email, telefone };
+        const contato = { nome, email, phone };
         handleGuardarContato(contato);
         setNome('');
         setEmail('');
-        setTelefone('');
+        setPhone('');
+        alert("Contato salvo com sucesso!");
     };
 
     return (
-        <div className="container">
-            <form onSubmit={handleSubmit}>
-
-                <input type="text" placeholder="Nome" value={nome} onChange={(e) => setNome(e.target.value)} required />
-                <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                <input type="tel" placeholder="Telefone" value={telefone} onChange={(e) => setTelefone(e.target.value)} required />
-                <button type="submit" className="bn632-hover bn26">ADICIONAR</button>
+        <div className="container d-flex justify-content-center align-items-center w-100">
+            <form onSubmit={handleSubmit} className="d-flex flex-column align-items-center">
+                <div className="form-group">
+                    <div className="input-container">
+                        <input type="text" className="form-control mb-3 w-100" value={nome} onChange={(e) => setNome(e.target.value)} required />
+                        <span className="input-label">Nome</span>
+                    </div>
+                    <div className="input-container">
+                        <input type="email" className="form-control mb-3 w-100" value={email} onChange={(e) => setEmail(e.target.value)} required />
+                        <span className="input-label">Email</span>
+                    </div>
+                    <div className="input-container">
+                        <input type="text" className="form-control mb-3 w-100" value={phone} onChange={(e) => setPhone(e.target.value)} required />
+                        <span className="input-label">Telefone</span>
+                    </div>
+                </div>
+                <button type="submit" className="btn btn-primary w-100">ADICIONAR</button>
             </form>
         </div>
+
+
+
+
     );
 };
 
